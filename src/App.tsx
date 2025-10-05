032e9b59c524bf0cab093d713bb1d8f406489f79
kleitonccb123 <sejadigitalmarketclientes@gmail.com>
2025-10-05 20:48:54 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import SellFluxForm from "@/components/SellFluxForm";
 import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
