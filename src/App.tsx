37750edf7d146fc6a4859ddf694a835c347b741e
kleitonccb123 <sejadigitalmarketclientes@gmail.com>
2025-10-05 20:39:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+<Route path="/" element={<><Index /><SellFluxForm /></>} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
