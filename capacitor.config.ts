13117aaf5081c51ea2da665494f7101ca41b2a5d
kleitonccb123 <sejadigitalmarketclientes@gmail.com>
2025-10-05 19:51:39 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.4ec224ed04e54ed7a24c3e9ca94d5c8a',
+  appName: 'price-pro-clarity-88',
+  webDir: 'dist',
+  server: {
+    url: 'https://4ec224ed-04e5-4ed7-a24c-3e9ca94d5c8a.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  },
+  bundledWebRuntime: false
+};
+
+export default config;
